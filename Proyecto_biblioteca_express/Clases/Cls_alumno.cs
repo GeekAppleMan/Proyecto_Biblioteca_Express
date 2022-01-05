4818a0e5d5706a066f272f8e1b9ddd8eb3a03737
GeekAppleMan <Jaimehbo13@hotmail.com>
2022-01-05 11:26:29 -0700

    update

--- Proyecto_biblioteca_express/Clases/Cls_alumno.cs
+++ Proyecto_biblioteca_express/Clases/Cls_alumno.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,31 @@ using System.Threading.Tasks;
 
 namespace Proyecto_biblioteca_express
 {
-    class Cls_alumno
+    class Cls_alumno : Cls_conexion
     {
 
         public void verifAlumno(int matricula)
         {
-            
+            string query = "SELECT * FROM tb_alumnos WHERE estatus = '1'";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            databaseConnection.Open();
+
+            reader = commandDatabase.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                   
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontro al alumno");
+            }
         }
     }
 }
